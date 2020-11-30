b7a065ff92516ed58a28e1bca94fdc96868b5ef7
naveens <joinnaveens@gmail.com>
2020-11-30 08:09:39 +0530

    job-application

---
+++ Tasks/job-app/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import FormContainer from './FormContainer'
+
+const App = (props) => {
+    return (
+        <div>
+            <FormContainer />
+        </div>
+    )
+}
+
+export default App
